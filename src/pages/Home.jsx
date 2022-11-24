317a366485a3a76576c7b38eeff0e9a61e2c46bd
Caio Fernandes <caiobart601@gmail.com>
2022-11-24 11:20:07 -0300

    Projeto finalizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
   return (
     <div className="container">
-      <h2 className="title">Melhores Filmes:</h2>
+      <h2 className="title">Melhor Avaliações: </h2>
       <div className="movies-container">
       {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
